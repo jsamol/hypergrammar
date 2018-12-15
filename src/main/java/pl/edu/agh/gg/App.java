8fe69aabab12bf413a8ad0c2d85b44c52b2fccd7
Julia Samól <julia.samol@gmail.com>
2018-12-15 18:05:21 +0100

    Add test case

--- src/main/java/pl/edu/agh/gg/App.java
+++ src/main/java/pl/edu/agh/gg/App.java
@@ -1,5 +1,12 @@
 package pl.edu.agh.gg;
 
+import pl.edu.agh.gg.data.*;
+import pl.edu.agh.gg.hypergraph.HyperEdge;
+import pl.edu.agh.gg.hypergraph.HyperEdgeType;
+import pl.edu.agh.gg.hypergraph.HyperGraph;
+import pl.edu.agh.gg.hypergraph.Vertex;
+import pl.edu.agh.gg.ui.HyperGraphDrawer;
+
 public class App {
 
     public static void main(String[] args) {
@@ -9,5 +16,22 @@ public class App {
 
     private void run() {
 
+        // Test case
+        HyperGraph<Vertex, HyperEdge<Vertex>> graph = new HyperGraph<>();
+
+        Vertex v1 = new Vertex(new Point(0, 0));
+        Vertex v2 = new Vertex(new Point(10, 0));
+        Vertex v3 = new Vertex(new Point(0, 10));
+        Vertex v4 = new Vertex(new Point(10, 10));
+
+        graph.addVertex(v1, v2, v3, v4);
+        graph.addEdge(new HyperEdge<>(HyperEdgeType.INTERIOR, v1, v2, v3, v4));
+        graph.addEdge(new HyperEdge<>(HyperEdgeType.BOUNDARY, v1, v2));
+        graph.addEdge(new HyperEdge<>(HyperEdgeType.BOUNDARY, v1, v3));
+        graph.addEdge(new HyperEdge<>(HyperEdgeType.BOUNDARY, v2, v4));
+        graph.addEdge(new HyperEdge<>(HyperEdgeType.BOUNDARY, v3, v4));
+
+        HyperGraphDrawer drawer = new HyperGraphDrawer(graph);
+        drawer.draw();
     }
 }
