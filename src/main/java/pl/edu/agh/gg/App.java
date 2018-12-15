b89a4d7eb1162792849ad0240338589c47034b13
Julia Samól <julia.samol@gmail.com>
2018-12-15 18:01:52 +0100

    Add main App class

--- src/main/java/pl/edu/agh/gg/App.java
+++ src/main/java/pl/edu/agh/gg/App.java
@@ -1,4 +1,13 @@
 package pl.edu.agh.gg;
 
 public class App {
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+
+    private void run() {
+
+    }
 }
